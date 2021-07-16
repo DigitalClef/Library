fe568366da2ac5b50d64aa2532102ce37500f433
Christian Alamillo <calam002@ucr.edu>
2021-07-15 19:37:49 -0700

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+const libraryBox = document.querySelector('.library-box');
+
+function Book(title, author, pageCount, read) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    const book1 = new Book('Harry Potter', 'J.K. Rowling', 320, true);
+    myLibrary.push(book1);
+}
+
+addBookToLibrary();
